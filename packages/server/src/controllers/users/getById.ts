6705ca04bfb5876c8d166241b407afa92a388f19
adnanavni <adnan.avni@hotmail.com>
2022-12-10 14:40:10 +0200

    added post get by id endpoint

--- packages/server/src/controllers/users/getById.ts
+++ packages/server/src/controllers/users/getById.ts
@@ -1,11 +1,15 @@
 import { RequestHandler } from 'express'
-import { path } from 'ramda'
+import { isNil, path } from 'ramda'
 import { User } from '../../models/User'
 
 export const getById: RequestHandler = async (req, res) => {
-  const id = path(['params', '_id'], req)
+  const id = path(['params', 'id'], req)
 
   const user = await User.findById(id)
+
+  if (isNil(user)) {
+    return res.status(404).send({ error: 'User does not exist' })
+  }
 
   res.send(user)
 }
