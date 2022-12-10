6b94e4bb4e8b1048d7b53cc97fe707ba8b7756c9
adnanavni <adnan.avni@hotmail.com>
2022-12-10 15:35:27 +0200

    add get posts endpoint

--- packages/server/src/routes/postRoutes.ts
+++ packages/server/src/routes/postRoutes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express'
 import { createPost } from '../controllers/posts/createPost'
 import { getPostById } from '../controllers/posts/getPostById'
+import { getPosts } from '../controllers/posts/getPosts'
 import { roleAuth } from '../middleware/roleAuth'
 import { ERole } from '../types'
 
 const postRouter = Router()
 
 postRouter.post('/posts', [roleAuth([ERole.User])], createPost)
+postRouter.get('/posts/', [], getPosts)
 postRouter.get('/posts/:id', [], getPostById)
+
 
 export default postRouter
