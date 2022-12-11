66e304d5562d21846885a119e6216ed2f378adf2
Arttu Pennanen <arttu@pennanen.org>
2022-12-11 21:00:53 +0200

    add basic layout

--- packages/app/src/layouts/RootLayout/index.jsx
+++ packages/app/src/layouts/RootLayout/index.jsx
@@ -1,14 +1,23 @@
-import { Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 import classes from './style.module.css'
 
 export const RootLayout = () => {
   return (
     <div className={classes.rootLayout}>
-      <nav>nav</nav>
+      <nav className={classes.nav}>
+        <NavLink to="/">
+          <div className={classes.homeLink}>
+
+            <img src="/64.png" alt=""></img>
+            <header>JOBEL</header>
+
+          </div>
+        </NavLink>
+      </nav>
       <main>
         <Outlet />
       </main>
-      <footer>footer</footer>
+      <footer></footer>
     </div>
   )
 }
