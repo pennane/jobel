090a3e4fbbf515d873c125f15f938cd1104ebd47
Arttu Pennanen <arttu@pennanen.org>
2022-12-11 19:01:28 +0200

    fix commentCount to actually work

--- packages/server/src/models/Post/index.ts
+++ packages/server/src/models/Post/index.ts
@@ -4,38 +4,38 @@ import { IPost } from './types'
 export const postSchema = new Schema<IPost>({
   userId: {
     type: Schema.Types.ObjectId,
-    ref: 'User'
+    ref: 'User',
   },
   voters: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   score: {
     type: Number,
     required: true,
-    default: 0
+    default: 0,
   },
   content: {
     type: String,
-    required: true
+    required: true,
   },
   timeStamp: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   color: {
     type: Number,
-    default: 0
+    default: 0,
   },
-  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
 })
 
 postSchema.set('toJSON', {
   transform: (_, returnedObject) => {
     delete returnedObject.__v
   },
+  virtuals: true,
 })
 
-postSchema.virtual("commentCount").get(function () {
+postSchema.virtual('commentCount').get(function () {
   return this.comments.length
 })
-
 
 export const Post = model<IPost>('Post', postSchema)
