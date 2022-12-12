615d45dc834f0953847e3b12ba0fcce9c31660ff
adnanavni <adnan.avni@hotmail.com>
2022-12-12 12:58:58 +0200

    better looking settingsview

--- packages/app/src/views/SettingsView/index.jsx
+++ packages/app/src/views/SettingsView/index.jsx
@@ -1,20 +1,26 @@
 import classes from './style.module.css'
 import { LoginForm } from '../../components/LoginForm'
-import { LogoutButton } from '../../components/LogoutButton'
 import { useAuthContext } from '../../hooks/useAuthContext'
 import { SignupForm } from '../../components/SignupForm'
+import { LogoutButton } from '../../components/LogoutButton'
+import { useState } from 'react'
 
 export const SettingsView = () => {
+
   const { isLoggedIn } = useAuthContext()
+  const [formVisibility, setFormVisibility] = useState(true)
+
+  const toggleSignup = () => {
+    setFormVisibility(!formVisibility)
+  }
 
   return (
     <div className={classes.mainView}>
       {isLoggedIn && <LogoutButton />}
       {!isLoggedIn && (
-        <>
-          <LoginForm />
-          <SignupForm />
-        </>
+        (formVisibility ?
+          <><LoginForm className={classes.loginForm} /> <button onClick={toggleSignup}>Sign up</button></>
+          : <> <SignupForm className={classes.signupForm} /> <button onClick={toggleSignup}>Log in</button></>)
       )}
     </div>
   )
