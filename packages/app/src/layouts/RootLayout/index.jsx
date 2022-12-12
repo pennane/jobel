df3b36c8dc69b6e81340247777e3211d2abf73b3
Roope Kylli <roope.kylli@metropolia.fi>
2022-12-12 13:51:35 +0200

    Added stuff to nav bar

--- packages/app/src/layouts/RootLayout/index.jsx
+++ packages/app/src/layouts/RootLayout/index.jsx
@@ -1,7 +1,10 @@
 import { NavLink, Outlet } from 'react-router-dom'
 import classes from './style.module.css'
+import {  useAuthContext} from '../../hooks/useAuthContext';
+
 
 export const RootLayout = () => {
+const {user, isLoggedIn} = useAuthContext()
   return (
     <div className={classes.rootLayout}>
       <nav className={classes.nav}>
@@ -11,10 +14,14 @@ export const RootLayout = () => {
             <header>JOBEL</header>
           </div>
         </NavLink>
-
+      <div className={classes.rightSide}>
+      <div>
+        {isLoggedIn && <div>logged in as {user.userName}</div>}
+        </div>
         <NavLink to="/settings">
           <div className={classes.navLink}>⚙️</div>
         </NavLink>
+      </div>
       </nav>
       <main>
         <Outlet />
