166146ce6f2c2ce3f806f1587d66b4eb49a46e9a
Arttu Pennanen <arttu@pennanen.org>
2022-12-14 20:29:59 +0200

    support adding votes better

--- packages/server/src/controllers/posts/getPostById.ts
+++ packages/server/src/controllers/posts/getPostById.ts
@@ -1,11 +1,38 @@
 import { RequestHandler } from 'express'
-import { path, isNil } from 'ramda'
+import { path, isNil, omit, pipe } from 'ramda'
 import { Post } from '../../models/Post'
+import { Document, Types } from 'mongoose'
+import { IPost } from '../../models/Post/types'
+
+const parseHasVoted = (userId?: Types.ObjectId) => (post: IPost & Document) => {
+  if (!userId) {
+    return post.toObject()
+  }
+
+  const comments = post.comments.map((m) => {
+    console.log(m, userId)
+    const obu = m.toObject()
+    const withoutVoters = omit(['voters'], obu)
+    return {
+      ...withoutVoters,
+      // @ts-expect-error asdf
+      hasVoted: m.voters.includes(userId),
+    }
+  })
+
+  return {
+    ...post.toObject(),
+    // @ts-expect-error asdf
+    hasVoted: post.voters.includes(userId),
+    comments,
+  }
+}
 
 const getId = path(['params', 'id'])
 
 export const getPostById: RequestHandler = async (req, res) => {
   const id = getId(req)
+  const userId = req.tokenPayload?._id
 
   const post = await Post.findById(id, {
     _id: 1,
@@ -16,13 +43,15 @@ export const getPostById: RequestHandler = async (req, res) => {
     score: 1,
     content: 1,
     visibleUserId: 1,
+    voters: 1,
   })
-    .populate('comments', '_id visibleUserId timeStamp content score')
+    .populate('comments', '_id visibleUserId timeStamp content score voters')
     .exec()
 
   if (isNil(post)) {
     return res.status(404).send({ error: 'Post does not exist' })
   }
+  const parsedPost = pipe(parseHasVoted(userId), omit(['voters']))(post)
 
-  res.send({ post })
+  res.send({ post: parsedPost })
 }
